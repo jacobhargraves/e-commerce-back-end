2af33f1746c4b93ff729be8891adcfadf768e8c1
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-17 09:32:51 -0600

    post route for category created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,8 +34,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
