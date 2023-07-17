ff5ba15aa42c10d5c8c8827e14df378a84750a1e
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-17 09:53:24 -0600

    put route added for category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,8 +44,28 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
