3391fe9ed7662a077cb660dc60424a81a10f276c
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-17 10:02:26 -0600

    delete route for categories is now created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,8 +68,20 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
